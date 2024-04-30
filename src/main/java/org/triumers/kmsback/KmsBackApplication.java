472a0c6871fffd88c359263a4f354d82052307a2
Leegiyeon <rldus3512@naver.com>
2024-04-30 10:57:08 +0900

    build(main): 프로젝트 초기 설정 파일 생성
    - CI: Test Code 실행 (main, develop 브랜치)
    - Build 파일 등록
    - .gitignore: CD를 위한 jar 파일 허용

---
+++ src/main/java/org/triumers/kmsback/KmsBackApplication.java
@@ -0,0 +1,13 @@
+package org.triumers.kmsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/triumers/kmsback/KmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.triumers.kmsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
