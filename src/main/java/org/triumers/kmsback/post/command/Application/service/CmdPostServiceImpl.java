5f862074b4c154582ef370f61f29857e64606159
hso <sh07202@naver.com>
2024-05-08 19:31:17 +0900

    feat(post) : 게시글 저장 테스트 코드 및 로직 작

--- src/main/java/org/triumers/kmsback/post/command/Application/service/CmdPostServiceImpl.java
+++ src/main/java/org/triumers/kmsback/post/command/Application/service/CmdPostServiceImpl.java
@@ -1,4 +1,32 @@
 package org.triumers.kmsback.post.command.Application.service;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.triumers.kmsback.post.command.Application.dto.CmdPostDTO;
+import org.triumers.kmsback.post.command.domain.aggregate.entity.CmdPost;
+import org.triumers.kmsback.post.command.domain.repository.CmdPostRepository;
+
+@Service
 public class CmdPostServiceImpl implements CmdPostService {
+
+    private final ModelMapper mapper;
+    private final CmdPostRepository cmdPostRepository;
+
+    @Autowired
+    public CmdPostServiceImpl(ModelMapper mapper, CmdPostRepository cmdPostRepository) {
+        this.mapper = mapper;
+        this.cmdPostRepository = cmdPostRepository;
+    }
+
+    @Override
+    public CmdPost registPost(CmdPostDTO post) {
+
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        CmdPost registPost = mapper.map(post, CmdPost.class);
+        System.out.println(registPost);
+
+        return cmdPostRepository.save(registPost);
+    }
 }
