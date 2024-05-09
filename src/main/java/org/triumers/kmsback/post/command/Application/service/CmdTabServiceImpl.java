609bf1831551f30f4ffad49013e46377c84ce1aa
hso <sh07202@naver.com>
2024-05-09 18:34:20 +0900

    feat(Post) : 탭 서비스 구현 및 테스트

--- src/main/java/org/triumers/kmsback/post/command/Application/service/CmdTabServiceImpl.java
+++ src/main/java/org/triumers/kmsback/post/command/Application/service/CmdTabServiceImpl.java
@@ -3,17 +3,22 @@ package org.triumers.kmsback.post.command.Application.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.triumers.kmsback.post.command.Application.dto.CmdJoinEmployeeDTO;
+import org.triumers.kmsback.post.command.Application.dto.CmdTabRelationDTO;
 import org.triumers.kmsback.post.command.domain.aggregate.entity.CmdJoinEmployee;
+import org.triumers.kmsback.post.command.domain.aggregate.entity.CmdTabRelation;
 import org.triumers.kmsback.post.command.domain.repository.CmdJoinEmployeeRepository;
+import org.triumers.kmsback.post.command.domain.repository.CmdTabRelationRepository;
 
 @Service
 public class CmdTabServiceImpl implements CmdTabService{
 
     private final CmdJoinEmployeeRepository cmdJoinEmployeeRepository;
+    private final CmdTabRelationRepository cmdTabRelationRepository;
 
     @Autowired
-    public CmdTabServiceImpl(CmdJoinEmployeeRepository cmdJoinEmployeeRepository) {
+    public CmdTabServiceImpl(CmdJoinEmployeeRepository cmdJoinEmployeeRepository, CmdTabRelationRepository cmdTabRelationRepository) {
         this.cmdJoinEmployeeRepository = cmdJoinEmployeeRepository;
+        this.cmdTabRelationRepository = cmdTabRelationRepository;
     }
 
     @Override
@@ -32,5 +37,18 @@ public class CmdTabServiceImpl implements CmdTabService{
         cmdJoinEmployeeRepository.deleteById(deleteEmployee.getId());
 
         return deleteEmployee;
+    }
+
+    @Override
+    public CmdTabRelation registTab(CmdTabRelationDTO tabRelation, int employeeId) {
+
+        CmdTabRelation newTab = new CmdTabRelation(tabRelation.getIsPublic(), tabRelation.getBottomTabId(),
+                                                   tabRelation.getTopTabId());
+        cmdTabRelationRepository.save(newTab);
+
+        CmdJoinEmployeeDTO joinEmployee = new CmdJoinEmployeeDTO(true, employeeId, newTab.getId());
+        addEmployeeTab(joinEmployee);
+
+        return newTab;
     }
 }
