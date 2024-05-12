b88b4b0be6fda642e94777a125f8ae11e4c7029d
noctesilente <mox9nox@gmail.com>
2024-05-13 01:07:07 +0900

    feat(anonymous-board): 컨트롤러 계층에서 게시글 조회/검색 예외 처리 기능 구현
    - 게시글 검색 시 잘못된 검색 유형이 주어졌을 경우 예외 처리
    - 게시글 조회 시 주어진 게시글 id에 해당하는 게시글이 없는 경우 예외 처리

--- src/main/java/org/triumers/kmsback/anonymousboard/query/controller/QryAnonymousBoardController.java
+++ src/main/java/org/triumers/kmsback/anonymousboard/query/controller/QryAnonymousBoardController.java
@@ -3,10 +3,13 @@ package org.triumers.kmsback.anonymousboard.query.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.triumers.kmsback.anonymousboard.query.dto.QryAnonymousBoardDTO;
 import org.triumers.kmsback.anonymousboard.query.service.QryAnonymousBoardService;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/anonymous-board")
@@ -36,9 +39,18 @@ public class QryAnonymousBoardController {
     @GetMapping("/{id}")
     public ResponseEntity<QryAnonymousBoardDTO> getAnonymousBoardById(@PathVariable int id) {
         QryAnonymousBoardDTO anonymousBoard = qryAnonymousBoardService.findAnonymousBoardById(id);
-        if (anonymousBoard == null) {
-            return ResponseEntity.notFound().build();
-        }
         return ResponseEntity.ok(anonymousBoard);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
