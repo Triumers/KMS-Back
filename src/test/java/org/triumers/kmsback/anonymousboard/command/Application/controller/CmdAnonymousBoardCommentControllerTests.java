fa55fc8152963079b1460ea220b01f4249863ad5
noctesilente <mox9nox@gmail.com>
2024-05-12 13:30:14 +0900

    refactor(anonymous-board): 게시글 댓글 조회 기능 마이바티스로 구현 후 jpa 코드 삭제

--- src/test/java/org/triumers/kmsback/anonymousboard/command/Application/controller/CmdAnonymousBoardCommentControllerTests.java
+++ src/test/java/org/triumers/kmsback/anonymousboard/command/Application/controller/CmdAnonymousBoardCommentControllerTests.java
@@ -7,23 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.triumers.kmsback.anonymousboard.command.Application.dto.CmdAnonymousBoardCommentDTO;
 import org.triumers.kmsback.anonymousboard.command.Application.service.CmdAnonymousBoardCommentService;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 
 @WebMvcTest(value = CmdAnonymousBoardCommentController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 class CmdAnonymousBoardCommentControllerTests {
@@ -39,25 +32,6 @@ class CmdAnonymousBoardCommentControllerTests {
     @BeforeEach
     void setUp() {
         objectMapper = new ObjectMapper();
-    }
-
-    // 댓글 목록 조회 테스트
-    @Test
-    void getAnonymousBoardCommentList_shouldReturnAnonymousBoardCommentDTOList() throws Exception {
-        int anonymousBoardId = 1;
-        List<CmdAnonymousBoardCommentDTO> anonymousBoardCommentDTOList = Arrays.asList(
-                new CmdAnonymousBoardCommentDTO(),
-                new CmdAnonymousBoardCommentDTO()
-        );
-        Page<CmdAnonymousBoardCommentDTO> anonymousBoardCommentDTOPage = new PageImpl<>(anonymousBoardCommentDTOList);
-
-        when(cmdAnonymousBoardCommentService.findAllAnonymousBoardComment(eq(anonymousBoardId), any(Pageable.class))).thenReturn(anonymousBoardCommentDTOPage);
-
-        mockMvc.perform(get("/anonymous-board/{anonymousBoardId}/comments", anonymousBoardId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content.length()").value(2));
-
-        verify(cmdAnonymousBoardCommentService, times(1)).findAllAnonymousBoardComment(eq(anonymousBoardId), any(Pageable.class));
     }
 
     // 댓글 작성 테스트
--- src/test/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardCommentServiceImplTests.java
+++ src/test/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardCommentServiceImplTests.java
@@ -6,16 +6,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.triumers.kmsback.anonymousboard.command.Application.dto.CmdAnonymousBoardCommentDTO;
 import org.triumers.kmsback.anonymousboard.command.domain.aggregate.entity.CmdAnonymousBoardComment;
 import org.triumers.kmsback.anonymousboard.command.domain.repository.CmdAnonymousBoardCommentRepository;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -51,22 +46,6 @@ public class CmdAnonymousBoardCommentServiceImplTests {
         cmdAnonymousBoardComment2.setCreatedDate(LocalDateTime.now());
         cmdAnonymousBoardComment2.setMacAddress("MAC2");
         cmdAnonymousBoardComment2.setAnonymousBoardId(1);
-    }
-
-    // 댓글 조회(페이징 처리까지)
-    @Test
-    void findAllAnonymousBoardComment_shouldReturnPageOfCmdAnonymousBoardCommentDTO() {
-        int anonymousBoardId = 1;
-        List<CmdAnonymousBoardComment> cmdAnonymousBoardCommentList = Arrays.asList(cmdAnonymousBoardComment1, cmdAnonymousBoardComment2);
-        Page<CmdAnonymousBoardComment> cmdAnonymousBoardCommentPage = new PageImpl<>(cmdAnonymousBoardCommentList);
-
-        when(cmdAnonymousBoardCommentRepository.findByAnonymousBoardId(eq(anonymousBoardId), any(Pageable.class))).thenReturn(cmdAnonymousBoardCommentPage);
-
-        Page<CmdAnonymousBoardCommentDTO> result = cmdAnonymousBoardCommentService.findAllAnonymousBoardComment(anonymousBoardId, Pageable.unpaged());
-
-        assertNotNull(result);
-        assertEquals(2, result.getTotalElements());
-        verify(cmdAnonymousBoardCommentRepository, times(1)).findByAnonymousBoardId(eq(anonymousBoardId), any(Pageable.class));
     }
 
     // 댓글 작성
