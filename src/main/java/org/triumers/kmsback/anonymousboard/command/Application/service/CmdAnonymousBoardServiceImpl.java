5bc93855d2d2013fd65d24db5ed0baab978e157a
noctesilente <mox9nox@gmail.com>
2024-05-12 13:16:29 +0900

    refactor(anonymous-board): 게시글 조회 기능 마이바티스로 구현 후 jpa 코드 삭제

--- src/main/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardServiceImpl.java
+++ src/main/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardServiceImpl.java
@@ -1,8 +1,6 @@
 package org.triumers.kmsback.anonymousboard.command.Application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.triumers.kmsback.anonymousboard.command.Application.dto.CmdAnonymousBoardDTO;
@@ -32,35 +30,7 @@ public class CmdAnonymousBoardServiceImpl implements CmdAnonymousBoardService {
         return dto;
     }
 
-    // 1. 게시글 조회(페이징 처리까지)
-    @Transactional(readOnly = true)
-    public Page<CmdAnonymousBoardDTO> findAllAnonymousBoard(Pageable pageable) {
-        Page<CmdAnonymousBoard> cmdAnonymousBoardPage = cmdAnonymousBoardRepository.findAll(pageable);
-        return cmdAnonymousBoardPage.map(this::convertToDto);
-    }
-
-    // 2-1. 제목으로 게시글 검색
-    @Transactional(readOnly = true)
-    public Page<CmdAnonymousBoardDTO> searchAnonymousBoardByTitle(String title, Pageable pageable) {
-        Page<CmdAnonymousBoard> cmdAnonymousBoardPage = cmdAnonymousBoardRepository.findByTitleContaining(title, pageable);
-        return cmdAnonymousBoardPage.map(this::convertToDto);
-    }
-
-    // 2-2. 내용으로 게시글 검색
-    @Transactional(readOnly = true)
-    public Page<CmdAnonymousBoardDTO> searchAnonymousBoardByContent(String content, Pageable pageable) {
-        Page<CmdAnonymousBoard> cmdAnonymousBoardPage = cmdAnonymousBoardRepository.findByContentContaining(content, pageable);
-        return cmdAnonymousBoardPage.map(this::convertToDto);
-    }
-
-    // 2-3. 제목+내용으로 게시글 검색
-    @Transactional(readOnly = true)
-    public Page<CmdAnonymousBoardDTO> searchAnonymousBoardByTitleAndContent(String keyword, Pageable pageable) {
-        Page<CmdAnonymousBoard> cmdAnonymousBoardPage = cmdAnonymousBoardRepository.findByTitleContainingOrContentContaining(keyword, keyword, pageable);
-        return cmdAnonymousBoardPage.map(this::convertToDto);
-    }
-
-    // 3. 게시글 작성
+    // 게시글 작성
     @Transactional
     public CmdAnonymousBoardDTO saveAnonymousBoard(CmdAnonymousBoardDTO cmdAnonymousBoardDTO) {
         try {
@@ -83,7 +53,7 @@ public class CmdAnonymousBoardServiceImpl implements CmdAnonymousBoardService {
         }
     }
 
-    // 4. 게시글 삭제
+    // 게시글 삭제
     @Transactional
     public void deleteAnonymousBoard(int id) {
         cmdAnonymousBoardRepository.deleteById(id);
