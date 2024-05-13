a0b51cec3076c56da24cd9d98f0d57089e75ba11
moomint8 <office05053@gmail.com>
2024-05-13 20:14:59 +0900

    test(auth): 잘못된 이메일 형식 예외 테스트 코드 구현
    잘못된 이메일 형식을 예외 처리하는 테스트 코드를 구현했습니다.

--- src/test/java/org/triumers/kmsback/auth/command/Application/service/AuthServiceTest.java
+++ src/test/java/org/triumers/kmsback/auth/command/Application/service/AuthServiceTest.java
@@ -2,18 +2,29 @@ package org.triumers.kmsback.auth.command.Application.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.triumers.kmsback.auth.command.Application.dto.AuthDTO;
 import org.triumers.kmsback.auth.command.domain.aggregate.enums.UserRole;
 import org.triumers.kmsback.auth.command.domain.repository.AuthRepository;
+import org.triumers.kmsback.common.exception.WrongInputTypeException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class AuthServiceTest {
+    private final String RIGHT_FORMAT_EMAIL = "testUnique@gmail.com";
+    private final String RIGHT_FORMAT_PASSWORD = "aAbB1234";
+    private final String RIGHT_FORMAT_NAME = "테스트유저";
+    private final String RIGHT_FORMAT_USER_ROLE = UserRole.ROLE_NORMAL.name();
+    private final String RIGHT_PHONE_NUMBER = "010-1234-5678";
 
     @Autowired
     private AuthService authService;
@@ -27,12 +38,33 @@ class AuthServiceTest {
 
         // given
         AuthDTO newEmployee =
-                new AuthDTO("testUnique@gmail.com", "aAbB1234", "테스트유저", null, UserRole.ROLE_NORMAL.name(), null, null, "010-1234-5678", 1, 1, 1);
+                new AuthDTO(RIGHT_FORMAT_EMAIL, RIGHT_FORMAT_PASSWORD, RIGHT_FORMAT_NAME, null,
+                        RIGHT_FORMAT_USER_ROLE, null, null, RIGHT_PHONE_NUMBER, 1, 1,
+                        1);
 
         // when
         assertDoesNotThrow(() -> authService.signup(newEmployee));
 
         // then
         assertNotNull(authRepository.findByEmail(newEmployee.getEmail()));
+    }
+
+    @DisplayName("이메일 형식이 잘못된 회원가입 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"nothing", "noAnnotation.com", "noPoint@gmail",
+                            "containKorean@지메일.com", "containSpecialChar@gmail.com!"})
+    void invalidEmailSignup(String wrongEmail) {
+
+        // given
+        AuthDTO newEmployee =
+                new AuthDTO(wrongEmail, RIGHT_FORMAT_PASSWORD, RIGHT_FORMAT_NAME, null,
+                        RIGHT_FORMAT_USER_ROLE, null, null, RIGHT_PHONE_NUMBER, 1, 1,
+                        1);
+
+        // when
+        assertThrows(WrongInputTypeException.class, () -> authService.signup(newEmployee));
+
+        // then
+        assertNull(authRepository.findByEmail(newEmployee.getEmail()));
     }
 }
