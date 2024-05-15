7e0770d2a662c581d191956819d866a358952f63
moomint8 <office05053@gmail.com>
2024-05-15 22:51:20 +0900

    test(auth): 비밀번호 수정 테스트 구현
    현재 로그인되어있는 계정의 비밀번호 변경 테스트가 구현되었습니다.

--- src/test/java/org/triumers/kmsback/auth/command/Application/service/AuthServiceTest.java
+++ src/test/java/org/triumers/kmsback/auth/command/Application/service/AuthServiceTest.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.triumers.kmsback.auth.command.Application.dto.AuthDTO;
+import org.triumers.kmsback.auth.command.Application.dto.PasswordDTO;
 import org.triumers.kmsback.auth.command.domain.aggregate.enums.UserRole;
 import org.triumers.kmsback.auth.command.domain.repository.AuthRepository;
+import org.triumers.kmsback.auth.command.domain.service.CustomUserDetailsService;
 import org.triumers.kmsback.common.exception.WrongInputTypeException;
+import org.triumers.kmsback.common.exception.WrongInputValueException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,15 +40,15 @@ class AuthServiceTest {
     @Autowired
     private AuthRepository authRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @DisplayName("회원가입 테스트")
     @Test
     void signup() {
 
         // given
-        AuthDTO newEmployee =
-                new AuthDTO(RIGHT_FORMAT_EMAIL, RIGHT_FORMAT_PASSWORD, RIGHT_FORMAT_NAME, null,
-                        RIGHT_FORMAT_USER_ROLE, null, null, RIGHT_PHONE_NUMBER, 1, 1,
-                        1);
+        AuthDTO newEmployee = createRightAuthDTO();
 
         // when
         assertDoesNotThrow(() -> authService.signup(newEmployee));
@@ -108,7 +116,7 @@ class AuthServiceTest {
     @DisplayName("휴대폰 번호 형식이 잘못된 회원가입 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"010-alph-1234", "010-!!!!-1234", "000-1234-5678", "01-1234-5678", "0100-1234-5678",
-                            "010-12-5678", "010-12345-6789", "010-1234-567", "010-1234-56789"})
+            "010-12-5678", "010-12345-6789", "010-1234-567", "010-1234-56789"})
     void invalidPhoneNumberSignup(String wrongNumber) {
 
         // given
@@ -122,5 +130,39 @@ class AuthServiceTest {
 
         // then
         assertNull(authRepository.findByEmail(newEmployee.getEmail()));
+    }
+
+    @DisplayName("비밀번호 변경 테스트")
+    @Test
+    void editPassword() throws WrongInputTypeException, WrongInputValueException {
+
+        // given
+        setSecurityContextHolderByUserName();
+        String newPassword = "NewPwd1234";
+        PasswordDTO passwordDTO = new PasswordDTO(RIGHT_FORMAT_PASSWORD, newPassword);
+
+        // when
+        assertDoesNotThrow(() -> authService.editPassword(passwordDTO));
+
+        // then
+        assertTrue(bCryptPasswordEncoder.matches(newPassword, authRepository.findByEmail(RIGHT_FORMAT_EMAIL).getPassword()));
+    }
+
+    private AuthDTO createRightAuthDTO() {
+        return new AuthDTO(RIGHT_FORMAT_EMAIL, RIGHT_FORMAT_PASSWORD, RIGHT_FORMAT_NAME, null,
+                RIGHT_FORMAT_USER_ROLE, null, null, RIGHT_PHONE_NUMBER, 1, 1,
+                1);
+    }
+
+    private void setUser() throws WrongInputTypeException {
+        authService.signup(createRightAuthDTO());
+    }
+
+    private void setSecurityContextHolderByUserName() throws WrongInputTypeException {
+        setUser();
+        CustomUserDetailsService customUserDetailsService = new CustomUserDetailsService(authRepository);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(RIGHT_FORMAT_EMAIL);
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities()));
     }
 }
