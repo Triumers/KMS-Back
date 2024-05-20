42b8ae50654b9583ec2b0bb69ae73f0763b6fbed
Leegiyeon <rldus3512@naver.com>
2024-05-20 15:32:13 +0900

    test(quiz): 정답 제출 기능 테스트 코드
    - Test 코드 구현 #28

---
+++ src/test/java/org/triumers/kmsback/quiz/command/Application/service/CmdAnswerSubmitterServiceImplTest.java
@@ -0,0 +1,32 @@
+package org.triumers.kmsback.quiz.command.Application.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.triumers.kmsback.quiz.command.domain.aggregate.vo.CmdRequestAnswerSubmitVo;
+import org.triumers.kmsback.quiz.command.domain.repository.CmdAnswerSubmitterRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CmdAnswerSubmitterServiceImplTest {
+
+    @Autowired
+    private CmdAnswerSubmitterService cmdAnswerSubmitterService;
+
+    @Autowired
+    private CmdAnswerSubmitterRepository cmdAnswerSubmitterRepository;
+
+    @DisplayName("정답 제출 테스트")
+    @Test
+    void submitAnswer() {
+        // given
+        CmdRequestAnswerSubmitVo answer= new CmdRequestAnswerSubmitVo(1, "테스트 답변", "테스트 참조", true, 1, 1);
+
+        // when, then
+        assertDoesNotThrow(() -> cmdAnswerSubmitterService.submitAnswer(answer));
+    }
+}
