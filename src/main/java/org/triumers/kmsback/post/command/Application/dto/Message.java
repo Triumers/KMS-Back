6b46ebc054680a7bcce28ca28f0875283d232719
hso <sh07202@naver.com>
2024-05-20 12:18:43 +0900

    bug(post) : 생성자 추가

--- src/main/java/org/triumers/kmsback/post/command/Application/dto/Message.java
+++ src/main/java/org/triumers/kmsback/post/command/Application/dto/Message.java
@@ -9,4 +9,8 @@ public class Message {
     private String role;
     private String content;
 
+    public Message(String role, String content) {
+        this.role = role;
+        this.content = content;
+    }
 }
