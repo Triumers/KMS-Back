15f98b410914b18e1c5abb4436a1c0f4bcccf454
noctesilente <mox9nox@gmail.com>
2024-05-21 15:32:38 +0900

    refactor(approval): 컨트롤러 계층에서 로그인한 사용자의 id를 가져오도록 코드 수정

--- src/main/java/org/triumers/kmsback/approval/query/controller/QryRequestApprovalController.java
+++ src/main/java/org/triumers/kmsback/approval/query/controller/QryRequestApprovalController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import org.triumers.kmsback.approval.query.dto.QryRequestApprovalWithEmployeeDTO;
 import org.triumers.kmsback.approval.query.dto.QryRequestApprovalInfoDTO;
 import org.triumers.kmsback.approval.query.service.QryRequestApprovalService;
+import org.triumers.kmsback.auth.command.Application.service.AuthService;
+import org.triumers.kmsback.common.exception.NotLoginException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,86 +17,84 @@ import java.util.List;
 public class QryRequestApprovalController {
 
     private final QryRequestApprovalService qryRequestApprovalService;
+    private final AuthService authService;
 
     @Autowired
-    public QryRequestApprovalController(QryRequestApprovalService qryRequestApprovalService) {
+    public QryRequestApprovalController(QryRequestApprovalService qryRequestApprovalService, AuthService authService) {
         this.qryRequestApprovalService = qryRequestApprovalService;
+        this.authService = authService;
+    }
+
+    // 현재 사용자의 ID를 얻는 헬퍼 메서드
+    private int getCurrentUserId() throws NotLoginException {
+        return authService.whoAmI().getId();
     }
 
     // 본인이 요청한 결재 단일 조회
-    // requesterId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 4로 통일
     @GetMapping("/{approvalId}")
-    public QryRequestApprovalWithEmployeeDTO findById(@RequestParam(value = "requesterId", defaultValue = "4") int requesterId,
-                                                      @PathVariable("approvalId") int approvalId) {
+    public QryRequestApprovalWithEmployeeDTO findById(@PathVariable("approvalId") int approvalId) throws NotLoginException {
+        int requesterId = getCurrentUserId();
         return qryRequestApprovalService.findById(requesterId, approvalId);
     }
 
     // 본인이 요청한 결재 전체 조회(페이징 처리)
-    // requesterId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 4로 통일
     @GetMapping
-    public List<QryRequestApprovalInfoDTO> findAll(@RequestParam(value = "requesterId", defaultValue = "4") int requesterId,
-                                                   @RequestParam(value = "page", defaultValue = "1") int page,
-                                                   @RequestParam(value = "size", defaultValue = "10") int size) {
+    public List<QryRequestApprovalInfoDTO> findAll(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                   @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int requesterId = getCurrentUserId();
         return qryRequestApprovalService.findAll(requesterId, page, size);
     }
 
     // 본인이 요청한 결재 유형별 조회(페이징 처리)
-    // requesterId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 4로 통일
     @GetMapping("/type/{typeId}")
-    public List<QryRequestApprovalInfoDTO> findByType(@RequestParam(value = "requesterId", defaultValue = "4") int requesterId,
-                                                      @PathVariable("typeId") int typeId,
+    public List<QryRequestApprovalInfoDTO> findByType(@PathVariable("typeId") int typeId,
                                                       @RequestParam(value = "page", defaultValue = "1") int page,
-                                                      @RequestParam(value = "size", defaultValue = "10") int size) {
+                                                      @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int requesterId = getCurrentUserId();
         return qryRequestApprovalService.findByType(requesterId, typeId, page, size);
     }
 
     // 본인이 요청한 결재 기간별 조회(페이징 처리)
-    // requesterId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 4로 통일
     @GetMapping("/date-range")
-    public List<QryRequestApprovalInfoDTO> findByDateRange(@RequestParam(value = "requesterId", defaultValue = "4") int requesterId,
-                                                           @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+    public List<QryRequestApprovalInfoDTO> findByDateRange(@RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
                                                            @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
                                                            @RequestParam(value = "page", defaultValue = "1") int page,
-                                                           @RequestParam(value = "size", defaultValue = "10") int size) {
+                                                           @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int requesterId = getCurrentUserId();
         return qryRequestApprovalService.findByDateRange(requesterId, startDate, endDate, page, size);
     }
 
-
     // 본인이 요청받은 결재 단일 조회
-    // approverId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 2로 통일
     @GetMapping("/received/{requestApprovalId}")
-    public QryRequestApprovalWithEmployeeDTO findReceivedById(@RequestParam(value = "approverId", defaultValue = "2") int approverId,
-                                                      @PathVariable("requestApprovalId") int requestApprovalId) {
+    public QryRequestApprovalWithEmployeeDTO findReceivedById(@PathVariable("requestApprovalId") int requestApprovalId) throws NotLoginException {
+        int approverId = getCurrentUserId();
         return qryRequestApprovalService.findReceivedById(approverId, requestApprovalId);
     }
 
     // 본인이 요청받은 결재 전체 조회(페이징 처리)
-    // approverId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 2로 통일
     @GetMapping("/received")
-    public List<QryRequestApprovalInfoDTO> findAllReceived(@RequestParam(value = "approverId", defaultValue = "2") int approverId,
-                                                   @RequestParam(value = "page", defaultValue = "1") int page,
-                                                   @RequestParam(value = "size", defaultValue = "10") int size) {
+    public List<QryRequestApprovalInfoDTO> findAllReceived(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                           @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int approverId = getCurrentUserId();
         return qryRequestApprovalService.findAllReceived(approverId, page, size);
     }
 
     // 본인이 요청받은 결재 유형별 조회(페이징 처리)
-    // approverId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 2로 통일
     @GetMapping("/received/type/{typeId}")
-    public List<QryRequestApprovalInfoDTO> findReceivedByType(@RequestParam(value = "approverId", defaultValue = "2") int approverId,
-                                                      @PathVariable("typeId") int typeId,
-                                                      @RequestParam(value = "page", defaultValue = "1") int page,
-                                                      @RequestParam(value = "size", defaultValue = "10") int size) {
+    public List<QryRequestApprovalInfoDTO> findReceivedByType(@PathVariable("typeId") int typeId,
+                                                              @RequestParam(value = "page", defaultValue = "1") int page,
+                                                              @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int approverId = getCurrentUserId();
         return qryRequestApprovalService.findReceivedByType(approverId, typeId, page, size);
     }
 
     // 본인이 요청받은 결재 기간별 조회(페이징 처리)
-    // approverId = 추후에 받아오는 아이디로 수정, 우선 테스트용이니 2로 통일
     @GetMapping("/received/date-range")
-    public List<QryRequestApprovalInfoDTO> findReceivedByDateRange(@RequestParam(value = "approverId", defaultValue = "2") int approverId,
-                                                           @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
-                                                           @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
-                                                           @RequestParam(value = "page", defaultValue = "1") int page,
-                                                           @RequestParam(value = "size", defaultValue = "10") int size) {
+    public List<QryRequestApprovalInfoDTO> findReceivedByDateRange(@RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+                                                                   @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
+                                                                   @RequestParam(value = "page", defaultValue = "1") int page,
+                                                                   @RequestParam(value = "size", defaultValue = "10") int size) throws NotLoginException {
+        int approverId = getCurrentUserId();
         return qryRequestApprovalService.findReceivedByDateRange(approverId, startDate, endDate, page, size);
     }
 }
