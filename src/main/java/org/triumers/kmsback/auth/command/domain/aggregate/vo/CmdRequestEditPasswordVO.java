353733c6035d668a64f568c7ef33cee932d8d051
moomint8 <office05053@gmail.com>
2024-05-23 16:19:44 +0900

    refactor(auth): NotNull 조건 미충족 시 메시지 추가
    NotNull 조건 미충족 시 반환할 메시지를 추가했습니다.

--- src/main/java/org/triumers/kmsback/auth/command/domain/aggregate/vo/CmdRequestEditPasswordVO.java
+++ src/main/java/org/triumers/kmsback/auth/command/domain/aggregate/vo/CmdRequestEditPasswordVO.java
@@ -8,10 +8,10 @@ import org.triumers.kmsback.common.util.RegularExpression;
 @Getter
 public class CmdRequestEditPasswordVO {
 
-    @NotBlank
+    @NotBlank(message = "이전 비밀번호는 필수 기입 정보입니다.")
     private String oldPassword;
 
-    @NotBlank
+    @NotBlank(message = "변경할 비밀번호는 필수 기입 정보입니다.")
     @Pattern(regexp = RegularExpression.PASSWORD,
              message = "비밀번호는 8~12자리의 숫자, 소문자, 대문자를 각각 한 개 이상 가져야 합니다.")
     private String newPassword;
