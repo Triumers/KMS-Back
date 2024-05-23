151427a541db74948bc51db0d49b18f5f61d6af9
noctesilente <mox9nox@gmail.com>
2024-05-23 15:49:50 +0900

    feat(approval): 결재 유형 추가 기능 구현

--- src/main/java/org/triumers/kmsback/approval/command/Application/controller/CmdRequestApprovalController.java
+++ src/main/java/org/triumers/kmsback/approval/command/Application/controller/CmdRequestApprovalController.java
@@ -1,11 +1,13 @@
 package org.triumers.kmsback.approval.command.Application.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.triumers.kmsback.approval.command.Application.dto.CmdApprovalRequestDTO;
+import org.triumers.kmsback.approval.command.Application.service.CmdApprovalTypeService;
 import org.triumers.kmsback.approval.command.Application.service.CmdRequestApprovalService;
 import org.triumers.kmsback.auth.command.Application.service.AuthService;
 import org.triumers.kmsback.common.exception.NotLoginException;
@@ -16,10 +18,12 @@ public class CmdRequestApprovalController {
 
     private final CmdRequestApprovalService cmdRequestApprovalService;
     private final AuthService authService; // AuthService 주입
+    private final CmdApprovalTypeService cmdApprovalTypeService;
 
-    public CmdRequestApprovalController(CmdRequestApprovalService cmdRequestApprovalService, AuthService authService) {
+    public CmdRequestApprovalController(CmdRequestApprovalService cmdRequestApprovalService, AuthService authService, CmdApprovalTypeService cmdApprovalTypeService) {
         this.cmdRequestApprovalService = cmdRequestApprovalService;
         this.authService = authService;
+        this.cmdApprovalTypeService = cmdApprovalTypeService;
     }
 
     // 현재 사용자의 ID를 얻는 헬퍼 메서드
@@ -36,6 +40,12 @@ public class CmdRequestApprovalController {
         } catch (NotLoginException e) {
             return ResponseEntity.status(401).build(); // Unauthorized 응답
         }
+    }
+
+    @PostMapping("/approval-type")
+    public ResponseEntity<String> addNewApprovalType(@RequestBody String type) {
+        cmdApprovalTypeService.addNewApprovalType(type);
+        return new ResponseEntity<>("New approval type added successfully", HttpStatus.OK);
     }
 
 }
