800fe9c9377efcbd7737b48f1e0a62ed781386f6
Leegiyeon <rldus3512@naver.com>
2024-05-24 09:11:01 +0900

    feat(answer): 사원ID로 답 제출내역 조회
    - Controller에 메소드 추가

--- src/main/java/org/triumers/kmsback/quiz/query/controller/QryAnswerSubmitterController.java
+++ src/main/java/org/triumers/kmsback/quiz/query/controller/QryAnswerSubmitterController.java
@@ -1,27 +1,30 @@
 package org.triumers.kmsback.quiz.query.controller;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.triumers.kmsback.quiz.query.dto.QryAnswerSubmitterDTO;
 import org.triumers.kmsback.quiz.query.service.QryAnswerSubmitterService;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/answer")
+@RequestMapping({"/answer"})
 public class QryAnswerSubmitterController {
-
     private QryAnswerSubmitterService qryAnswerSubmitterService;
 
     public QryAnswerSubmitterController(QryAnswerSubmitterService qryAnswerSubmitterService) {
         this.qryAnswerSubmitterService = qryAnswerSubmitterService;
     }
 
-    /* 설명. 문제 ID로 문제 제출자 조회 */
-    @GetMapping("/submitter")
+    @GetMapping({"/submitter"})
     public List<QryAnswerSubmitterDTO> findQuizByIdAnswerSubmitter(int quizId) {
-        List<QryAnswerSubmitterDTO> qryAnswerSubmitterDTOs = qryAnswerSubmitterService.findSubmitterByQuizId(quizId);
+        List<QryAnswerSubmitterDTO> qryAnswerSubmitterDTOs = this.qryAnswerSubmitterService.findSubmitterByQuizId(quizId);
+        return qryAnswerSubmitterDTOs;
+    }
+
+    @GetMapping({"/count"})
+    public List<QryAnswerSubmitterDTO> findSubmitByEmployeeId(int employeeId) {
+        List<QryAnswerSubmitterDTO> qryAnswerSubmitterDTOs = this.qryAnswerSubmitterService.findSubmitByEmployeeId(employeeId);
         return qryAnswerSubmitterDTOs;
     }
 }
