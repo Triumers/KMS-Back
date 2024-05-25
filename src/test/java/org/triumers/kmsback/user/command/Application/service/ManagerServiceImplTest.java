72d4e6576e1bfebe2a318b1ce5f541e86237f59d
moomint8 <office05053@gmail.com>
2024-05-25 23:28:45 +0900

    test(user): Manager 회원 추가 기능(Default 비밀번호) 테스트 구현
    직원의 계정을 생성할 ManagerService에서 회원 추가 기능을 테스트하는 코드가 생성되었습니다.
    해당 테스트 코드는 비밀번호를 따로 기입하지 않았을 시 Default 값으로 비밀번호가 저장되는지를 테스트합니다.

--- src/test/java/org/triumers/kmsback/user/command/Application/service/ManagerServiceImplTest.java
+++ src/test/java/org/triumers/kmsback/user/command/Application/service/ManagerServiceImplTest.java
@@ -3,24 +3,31 @@ package org.triumers.kmsback.user.command.Application.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.triumers.kmsback.common.TestUserInfo;
 import org.triumers.kmsback.user.command.Application.dto.ManageUserDTO;
 import org.triumers.kmsback.user.command.domain.repository.EmployeeRepository;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class ManagerServiceImplTest {
+
+    @Value("${password}")
+    private String DEFAULT_PASSWORD;
 
     @Autowired
     private ManagerService managerService;
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @DisplayName("회원가입 테스트")
     @Test
@@ -36,5 +43,21 @@ class ManagerServiceImplTest {
 
         // then
         assertNotNull(employeeRepository.findByEmail(TestUserInfo.EMAIL));
+    }
+
+    @DisplayName("기본 비밀번호로 회원가입 테스트")
+    @Test
+    void signupWithDefaultPassword() {
+
+        // given
+        ManageUserDTO userDTO = new ManageUserDTO(TestUserInfo.EMAIL, null, TestUserInfo.NAME, null,
+                TestUserInfo.USER_ROLE, null, null, TestUserInfo.PHONE_NUMBER, 1, 1,
+                1);
+
+        // when
+        assertDoesNotThrow(() -> managerService.signup(userDTO));
+
+        // then
+        assertTrue(bCryptPasswordEncoder.matches(DEFAULT_PASSWORD, employeeRepository.findByEmail(TestUserInfo.EMAIL).getPassword()));
     }
 }
