48696be76efac4fdae22d6cb5a869b3e2aacbc15
moomint8 <office05053@gmail.com>
2024-05-26 06:23:20 +0900

    feat(manager): 유저 권한 수정 기능 구현
    유저의 권한을 수정하는 기능이 구현되었습니다.

--- src/main/java/org/triumers/kmsback/user/command/Application/controller/ManagerController.java
+++ src/main/java/org/triumers/kmsback/user/command/Application/controller/ManagerController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.triumers.kmsback.common.exception.NotAuthorizedException;
 import org.triumers.kmsback.user.command.Application.dto.ManageUserDTO;
 import org.triumers.kmsback.user.command.Application.service.ManagerService;
+import org.triumers.kmsback.user.command.domain.aggregate.vo.CmdRequestEditUserVO;
 import org.triumers.kmsback.user.command.domain.aggregate.vo.CmdRequestSignupUserVO;
 import org.triumers.kmsback.user.command.domain.aggregate.vo.CmdResponseMessageVO;
 
@@ -47,5 +49,24 @@ public class ManagerController {
 
         return ResponseEntity.status(HttpStatus.OK).body(
                 new CmdResponseMessageVO(newEmployee.getName() + " 회원 추가 성공"));
+    }
+
+    @PostMapping("/edit/role")
+    public ResponseEntity<CmdResponseMessageVO> editUserRole(@Valid @RequestBody CmdRequestEditUserVO request) {
+
+        ManageUserDTO targetEmployee = new ManageUserDTO();
+
+        targetEmployee.setEmail(request.getEmail());
+        targetEmployee.setRole(request.getRole());
+
+        try {
+            managerService.editUserRole(targetEmployee);
+
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new CmdResponseMessageVO("권한 수정 성공"));
+        } catch (NotAuthorizedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                    new CmdResponseMessageVO(e.getMessage()));
+        }
     }
 }
--- src/main/java/org/triumers/kmsback/user/command/Application/service/ManagerServiceImpl.java
+++ src/main/java/org/triumers/kmsback/user/command/Application/service/ManagerServiceImpl.java
@@ -2,12 +2,19 @@ package org.triumers.kmsback.user.command.Application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.triumers.kmsback.common.exception.NotAuthorizedException;
 import org.triumers.kmsback.user.command.Application.dto.ManageUserDTO;
 import org.triumers.kmsback.user.command.domain.aggregate.entity.Employee;
 import org.triumers.kmsback.user.command.domain.aggregate.enums.UserRole;
 import org.triumers.kmsback.user.command.domain.repository.EmployeeRepository;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Service
 public class ManagerServiceImpl implements ManagerService {
@@ -43,10 +50,64 @@ public class ManagerServiceImpl implements ManagerService {
         employeeRepository.save(employee);
     }
 
+    @Override
+    public void editUserRole(ManageUserDTO userDTO) throws NotAuthorizedException {
+        Employee employee = employeeRepository.findByEmail(userDTO.getEmail());
+
+        // 변경할 대상 권한이 자신의 권한이하인지 검증
+        validateRole(employee);
+
+        // 변경할 권한이 자신의 권한을 초과하지 않는지 검증
+        if (isNotEnoughRole(userDTO.getRole(), getCurrentRole())) {
+            throw new NotAuthorizedException();
+        }
+
+        employee.setUserRole(UserRole.valueOf(userDTO.getRole()));
+
+        employeeRepository.save(employee);
+    }
+
     private String passwordEncoder(String password) {
         if (password != null) {
             return bCryptPasswordEncoder.encode(password);
         }
         return bCryptPasswordEncoder.encode(DEFAULT_PASSWORD);
+    }
+
+    // 현재 로그인한 회원 권한
+    private String getCurrentRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        return auth.getAuthority();
+    }
+
+    private void validateRole(Employee targetUser) throws NotAuthorizedException {
+
+        // 자신을 초과하는 권한인 경우
+        if (isNotEnoughRole(targetUser.getUserRole().name(), getCurrentRole())) {
+            throw new NotAuthorizedException();
+        }
+    }
+
+    // 자신의 권한을 초과하는 경우 true 반환
+    private boolean isNotEnoughRole(String targetRole, String currentRole) {
+        if (targetRole.equals(currentRole)) {
+            return false;
+        }
+        if (targetRole.equals(UserRole.ROLE_HR_MANAGER.name())) {
+            return !currentRole.equals(UserRole.ROLE_ADMIN.name());
+        }
+        if (targetRole.equals(UserRole.ROLE_LEADER.name())) {
+            return !currentRole.equals(UserRole.ROLE_ADMIN.name()) &&
+                    !currentRole.equals(UserRole.ROLE_HR_MANAGER.name());
+        }
+        if (targetRole.equals(UserRole.ROLE_NORMAL.name())) {
+            return !currentRole.equals(UserRole.ROLE_ADMIN.name()) &&
+                    !currentRole.equals(UserRole.ROLE_HR_MANAGER.name()) &&
+                    !currentRole.equals(UserRole.ROLE_LEADER.name());
+        }
+        return true;
     }
 }
