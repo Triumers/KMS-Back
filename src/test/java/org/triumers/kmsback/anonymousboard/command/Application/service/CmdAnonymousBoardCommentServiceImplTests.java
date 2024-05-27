a9d170cfcb99fcac5e68650c5f1959930e1415e4
noctesilente <mox9nox@gmail.com>
2024-05-27 11:15:25 +0900

    refactor(anonymous-board): 테스트 코드 클래스 명 수정

--- src/test/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardCommentServiceImplTests.java
+++ src/test/java/org/triumers/kmsback/anonymousboard/command/Application/service/CmdAnonymousBoardCommentServiceImplTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class CmdAnonymousBoardCommentServiceImplTest {
+class CmdAnonymousBoardCommentServiceImplTests {
 
     @Autowired
     private CmdAnonymousBoardCommentRepository cmdAnonymousBoardCommentRepository;
