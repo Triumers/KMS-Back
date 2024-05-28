3ac924296782c761156302b9a20ca7250e1df696
Leegiyeon <rldus3512@naver.com>
2024-05-28 20:18:52 +0900

    refactor(answer): 경로 변경 및 변수 지정

--- src/main/java/org/triumers/kmsback/quiz/query/controller/QryQuizController.java
+++ src/main/java/org/triumers/kmsback/quiz/query/controller/QryQuizController.java
@@ -1,6 +1,7 @@
 package org.triumers.kmsback.quiz.query.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.triumers.kmsback.quiz.query.dto.QryQuizDTO;
@@ -19,15 +20,15 @@ public class QryQuizController {
     }
 
     /* 설명. 퀴즈상태로 퀴즈목록 조회 */
-    @GetMapping("/status")
-    public List<QryQuizDTO> findQuizByStatus(boolean status) {
+    @GetMapping("/status/{status}")
+    public List<QryQuizDTO> findQuizByStatus(@PathVariable boolean status) {
         List<QryQuizDTO> qryQuizDTOs = qryQuizService.findQuizByStatus(status);
         return qryQuizDTOs;
     }
 
     /* 설명. 퀴즈 ID로 퀴즈 문제, 정답, 해설, 참조 게시글 조회 */
-    @GetMapping("/id")
-    public QryQuizDTO findQuizById(int id) {
+    @GetMapping("/{id}")
+    public QryQuizDTO findQuizById(@PathVariable int id) {
         QryQuizDTO qryQuizDTO = qryQuizService.findQuizById(id);
         return qryQuizDTO;
     }
