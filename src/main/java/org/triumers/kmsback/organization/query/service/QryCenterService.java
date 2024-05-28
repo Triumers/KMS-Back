983615bc3af9e9ff229759a12f78e31ef2adf840
moomint8 <office05053@gmail.com>
2024-05-28 21:29:53 +0900

    test(organization): 본부 이름으로 검색 테스트 구현
    본부 이름을 이용해 검색하는 테스트가 구현되었습니다.

--- src/main/java/org/triumers/kmsback/organization/query/service/QryCenterService.java
+++ src/main/java/org/triumers/kmsback/organization/query/service/QryCenterService.java
@@ -2,6 +2,10 @@ package org.triumers.kmsback.organization.query.service;
 
 import org.triumers.kmsback.organization.query.dto.QryCenterDTO;
 
+import java.util.List;
+
 public interface QryCenterService {
     QryCenterDTO findCenterById(int id);
+
+    List<QryCenterDTO> findCenterListByName(String name);
 }
--- src/test/java/org/triumers/kmsback/organization/query/service/QryCenterServiceTest.java
+++ src/test/java/org/triumers/kmsback/organization/query/service/QryCenterServiceTest.java
@@ -9,6 +9,9 @@ import org.triumers.kmsback.organization.command.Application.dto.CmdCenterDTO;
 import org.triumers.kmsback.organization.command.Application.service.CmdCenterService;
 import org.triumers.kmsback.organization.query.dto.QryCenterDTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class QryCenterServiceTest {
 
     @Autowired
-    private QryCenterServiceImpl qryCenterService;
+    private QryCenterService qryCenterService;
 
     @Autowired
     private CmdCenterService cmdCenterService;
@@ -34,6 +37,32 @@ class QryCenterServiceTest {
 
         // then
         assertEquals(targetName, qryCenterDTO.getName());
+    }
+
+    @DisplayName("본부 검색 By 본부 이름")
+    @Test
+    void findCenterListByName() {
+
+        // given
+        List<String> names = new ArrayList<>();
+        String target = "UniqWord";
+
+        names.add(target);
+        names.add(target + "테스트");
+        names.add("테스트" + target);
+        names.add("테스트" + target + "테스트");
+
+        for (String name : names) {
+            addCenterForTest(name);
+        }
+
+        // when
+        List<QryCenterDTO> resultList = qryCenterService.findCenterListByName(target);
+
+        // then
+        for (QryCenterDTO result : resultList) {
+            assertTrue(result.getName().contains(target));
+        }
     }
 
     private int addCenterForTest(String centerName) {
