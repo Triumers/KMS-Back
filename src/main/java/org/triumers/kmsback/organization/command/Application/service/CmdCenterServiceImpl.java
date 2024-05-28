49aa63b3e57e49aa532ee84ac6eef36320d34140
moomint8 <office05053@gmail.com>
2024-05-28 20:53:33 +0900

    feat(organization): 본부 생성, 수정, 삭제 기능 구현
    본부를 생성, 수정(이름), 삭제 하는 기능이 구현되었습니다.

---
+++ src/main/java/org/triumers/kmsback/organization/command/Application/service/CmdCenterServiceImpl.java
@@ -0,0 +1,38 @@
+package org.triumers.kmsback.organization.command.Application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.triumers.kmsback.organization.command.Application.dto.CmdCenterDTO;
+import org.triumers.kmsback.organization.command.domain.aggregate.entity.CmdCenter;
+import org.triumers.kmsback.organization.command.domain.repository.CenterRepository;
+
+@Service
+public class CmdCenterServiceImpl implements CmdCenterService {
+
+    private final CenterRepository centerRepository;
+
+    @Autowired
+    public CmdCenterServiceImpl(CenterRepository centerRepository) {
+        this.centerRepository = centerRepository;
+    }
+
+    @Override
+    public int addCenter(CmdCenterDTO centerDTO) {
+        CmdCenter center = new CmdCenter();
+        center.setName(centerDTO.getName());
+        return centerRepository.save(center).getId();
+    }
+
+    @Override
+    public void editCenterName(CmdCenterDTO centerDTO) {
+        CmdCenter center = centerRepository.findById(centerDTO.getId());
+        center.setName(centerDTO.getName());
+        centerRepository.save(center);
+    }
+
+    @Override
+    public void removeCenter(CmdCenterDTO centerDTO) {
+        CmdCenter center = centerRepository.findById(centerDTO.getId());
+        centerRepository.deleteById(center.getId());
+    }
+}
