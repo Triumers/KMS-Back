78164fe9f29e393df3276c382aecf89021ceabb5
noctesilente <mox9nox@gmail.com>
2024-05-28 10:50:48 +0900

    test(approval): auth service 서비스 계층에서 사용하는 식으로 테스트 코드 변경

--- src/test/java/org/triumers/kmsback/approval/query/service/QryRequestApprovalServiceImplTests.java
+++ src/test/java/org/triumers/kmsback/approval/query/service/QryRequestApprovalServiceImplTests.java
@@ -1,11 +1,25 @@
 package org.triumers.kmsback.approval.query.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.triumers.kmsback.approval.command.Application.dto.CmdApprovalRequestDTO;
+import org.triumers.kmsback.approval.command.Application.service.CmdRequestApprovalService;
+import org.triumers.kmsback.approval.command.domain.aggregate.entity.CmdApproval;
+import org.triumers.kmsback.approval.command.domain.aggregate.entity.CmdRequestApproval;
+import org.triumers.kmsback.approval.command.domain.repository.CmdApprovalRepository;
+import org.triumers.kmsback.approval.command.domain.repository.CmdRequestApprovalRepository;
 import org.triumers.kmsback.approval.query.dto.QryRequestApprovalInfoDTO;
 import org.triumers.kmsback.approval.query.dto.QryRequestApprovalWithEmployeeDTO;
+import org.triumers.kmsback.common.LoggedInUser;
+import org.triumers.kmsback.common.TestUserInfo;
+import org.triumers.kmsback.common.exception.NotLoginException;
+import org.triumers.kmsback.user.command.Application.dto.ManageUserDTO;
+import org.triumers.kmsback.user.command.Application.service.AuthService;
+import org.triumers.kmsback.user.command.Application.service.ManagerService;
+import org.triumers.kmsback.user.command.domain.repository.EmployeeRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,214 +34,322 @@ class QryRequestApprovalServiceImplTests {
     @Autowired
     private QryRequestApprovalService qryRequestApprovalService;
 
+    @Autowired
+    private CmdApprovalRepository approvalRepository;
+
+    @Autowired
+    private CmdRequestApprovalRepository requestApprovalRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CmdRequestApprovalService cmdRequestApprovalService;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private LoggedInUser loggedInUser;
+
+    @Autowired
+    private ManagerService managerService;
+
+    @BeforeEach
+    public void setup() {
+        // 기존 데이터 삭제
+        requestApprovalRepository.deleteAll();
+        approvalRepository.deleteAll();
+    }
+
     @Test
-    public void findByIdTest() {
-        int requesterId = 4;
-        int approvalId = 1;
+    public void findByIdTest() throws NotLoginException {
+        // given
+        requesterSetUp();
+        int requesterId = authService.whoAmI().getId();
+        int approverId = employeeRepository.findByEmail(TestUserInfo.HR_MANAGER_EMAIL).getId();
 
-        QryRequestApprovalWithEmployeeDTO result = qryRequestApprovalService.findById(requesterId, approvalId);
+        // when
+        List<CmdApproval> approvals = approvalRepository.findAll();
+        int approvalId = approvals.get(0).getId();
+        QryRequestApprovalWithEmployeeDTO result = qryRequestApprovalService.findById(approvalId);
 
+        // then
         assertEquals(approvalId, result.getApprovalInfo().getApprovalId());
         assertEquals(requesterId, result.getRequester().getId());
-
-        assertEquals(3, result.getApprovalInfo().getApproverId());
-        assertEquals(2, result.getApprovalInfo().getApprovalOrder());
-        assertEquals("content 1", result.getApprovalInfo().getContent());
+        assertEquals(approverId, result.getApprovalInfo().getApproverId());
+        assertEquals(1, result.getApprovalInfo().getApprovalOrder());
+        assertEquals("Test Content", result.getApprovalInfo().getContent());
         assertEquals("워크스페이스 생성 요청", result.getApprovalInfo().getType());
-        assertEquals(LocalDateTime.of(2024, 5, 27, 17, 2, 38), result.getApprovalInfo().getCreatedAt());
-
     }
 
     @Test
-    public void findAllTest() {
-        int requesterId = 4;
+    public void findAllTest() throws NotLoginException {
+        // given
         int page = 1;
         int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        requesterSetUp();
+        int requesterId = authService.whoAmI().getId();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findAll(requesterId, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findAll(page, size);
 
-        int expectedTotalCount = 5; // 예상되는 총 결과 개수
+        // then
+        int expectedTotalCount = 4; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-
-        // 추가적인 검증 로직
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getRequesterId() == requesterId));
-
     }
 
     @Test
-    public void findByTypeTest() {
-        int requesterId = 4;
+    public void findByTypeTest() throws NotLoginException {
+        // given
+        int page = 1;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
         int typeId = 1;
-        int page = 1;
-        int size = 10;
+        requesterSetUp();
+        int requesterId = authService.whoAmI().getId();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByType(requesterId, typeId, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByType(typeId, page, size);
 
-        int expectedTotalCount = 2; // 예상되는 총 결과 개수
-        assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-
-        assertEquals(requesterId, approvalInfoDTOS.get(0).getRequesterId());
-        assertEquals(typeId, approvalInfoDTOS.get(0).getTypeId());
-        assertEquals(typeId, approvalInfoDTOS.get(1).getTypeId());
-        String expectedType = "워크스페이스 생성 요청"; // typeId에 해당하는 실제 type 값을 가져와서 사용
-        assertEquals(expectedType, approvalInfoDTOS.get(0).getType());
-        assertEquals(expectedType, approvalInfoDTOS.get(1).getType());
-    }
-
-    @Test
-    public void findByDateRangeTest() {
-        int requesterId = 4;
-        LocalDateTime startDate = LocalDateTime.of(2024, 4, 1, 0, 0);
-        LocalDateTime endDate = LocalDateTime.of(2024, 6, 30, 23, 59);
-        int page = 1;
-        int size = 10;
-
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByDateRange(requesterId, startDate, endDate, page, size);
-
+        // then
         int expectedTotalCount = 3; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-        assertEquals(requesterId, approvalInfoDTOS.get(0).getRequesterId());
-        assertEquals(requesterId, approvalInfoDTOS.get(1).getRequesterId());
-        assertEquals(requesterId, approvalInfoDTOS.get(2).getRequesterId());
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getRequesterId() == requesterId));
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getTypeId() == typeId));
+        String expectedType = "워크스페이스 생성 요청"; // typeId에 해당하는 실제 type 값을 가져와서 사용
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getType().equals(expectedType)));
+    }
 
+    @Test
+    public void findByDateRangeTest() throws NotLoginException {
+        // given
+        int page = 1;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        LocalDateTime startDate = LocalDateTime.of(2024, 4, 1, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2024, 6, 30, 23, 59);
+        requesterSetUp();
+        int requesterId = authService.whoAmI().getId();
+
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByDateRange(startDate, endDate, page, size);
+
+        // then
+        int expectedTotalCount = 4; // 예상되는 총 결과 개수
+        assertEquals(expectedTotalCount, approvalInfoDTOS.size());
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getRequesterId() == requesterId));
         assertTrue(approvalInfoDTOS.stream().allMatch(dto ->
                 dto.getCreatedAt().isAfter(startDate) && dto.getCreatedAt().isBefore(endDate)
         ));
     }
 
     @Test
-    public void findReceivedByIdTest() {
-        int approverId = 2;
-        int requestApprovalId = 1;
+    public void findReceivedByIdTest() throws NotLoginException {
+        // given
+        approverSetUp();
+        int approverId = authService.whoAmI().getId();
 
-        QryRequestApprovalWithEmployeeDTO result = qryRequestApprovalService.findReceivedById(approverId, requestApprovalId);
+        // when
+        List<CmdRequestApproval> requestApprovals = requestApprovalRepository.findAll();
+        int requestApprovalId = requestApprovals.get(0).getId();
+        QryRequestApprovalWithEmployeeDTO result = qryRequestApprovalService.findReceivedById(requestApprovalId);
 
+        // then
         assertEquals(requestApprovalId, result.getApprovalInfo().getRequestApprovalId());
         assertEquals(approverId, result.getApprover().getId());
-
-        assertEquals(2, result.getApprovalInfo().getApproverId());
-        assertEquals("content 1", result.getApprovalInfo().getContent());
-        assertEquals("워크스페이스 생성 요청", result.getApprovalInfo().getType());
-        assertEquals(LocalDateTime.of(2024, 5, 27, 17, 2, 38), result.getApprovalInfo().getCreatedAt());
-
+        assertEquals(approverId, result.getApprovalInfo().getApproverId());
+        assertEquals("Test Content", result.getApprovalInfo().getContent());
+        assertEquals("스터디 생성 요청", result.getApprovalInfo().getType());
     }
 
     @Test
-    public void findAllReceivedTest() {
-        int approverId = 2;
+    public void findAllReceivedTest() throws NotLoginException {
+        // given
         int page = 1;
-        int size = 10;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        approverSetUp();
+        int approverId = authService.whoAmI().getId();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findAllReceived(approverId, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findAllReceived(page, size);
 
+        // then
+        int expectedTotalCount = 4; // 예상되는 총 결과 개수
+        assertEquals(expectedTotalCount, approvalInfoDTOS.size());
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getApproverId() == approverId));
+    }
+
+    @Test
+    public void findReceivedByTypeTest() throws NotLoginException {
+        // given
+        int page = 1;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        int typeId = 2;
+        approverSetUp();
+
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByType(typeId, page, size);
+
+        // then
         int expectedTotalCount = 3; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-
-        // 추가적인 검증 로직
-        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getApproverId() == approverId));
-
-    }
-
-    @Test
-    public void findReceivedByTypeTest() {
-        int approverId = 2;
-        int typeId = 1;
-        int page = 1;
-        int size = 10;
-
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByType(approverId, typeId, page, size);
-
-        int expectedTotalCount = 2; // 예상되는 총 결과 개수
-        assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-
-        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getApproverId() == approverId));
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getTypeId() == typeId));
-
-        String expectedType = "워크스페이스 생성 요청"; // typeId에 해당하는 실제 type 값을 가져와서 사용
-        assertEquals(expectedType, approvalInfoDTOS.get(0).getType());
-        assertEquals(expectedType, approvalInfoDTOS.get(1).getType());
-
-        assertEquals("content 1", approvalInfoDTOS.get(0).getContent());
-        assertEquals("content 2", approvalInfoDTOS.get(1).getContent());
+        String expectedType = "스터디 생성 요청"; // typeId에 해당하는 실제 type 값을 가져와서 사용
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getType().equals(expectedType)));
+        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getContent().equals("Test Content")));
     }
 
     @Test
-    public void findReceivedByDateRangeTest() {
-        int approverId = 2;
+    public void findReceivedByDateRangeTest() throws NotLoginException {
+        // given
+        int page = 1;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
         LocalDateTime startDate = LocalDateTime.of(2024, 4, 1, 0, 0);
         LocalDateTime endDate = LocalDateTime.of(2024, 6, 30, 23, 59);
-        int page = 1;
-        int size = 10;
+        approverSetUp();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByDateRange(approverId, startDate, endDate, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByDateRange(startDate, endDate, page, size);
 
-        int expectedTotalCount = 2; // 예상되는 총 결과 개수
+        // then
+        int expectedTotalCount = 4; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
-
-        // 추가적인 검증 로직
-        assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getApproverId() == approverId));
-
         assertTrue(approvalInfoDTOS.stream().allMatch(dto ->
                 dto.getCreatedAt().isAfter(startDate) && dto.getCreatedAt().isBefore(endDate)
         ));
     }
 
-
     @Test
-    public void findByContentTest() {
-        int requesterId = 4;
-        String keyword = "content";
+    public void findByContentTest() throws NotLoginException {
+        // given
         int page = 1;
-        int size = 10;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        String keyword = "Content";
+        requesterSetUp();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByContent(requesterId, keyword, page, size);
-        System.out.println("approvalInfoDTOS = " + approvalInfoDTOS);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByContent(keyword, page, size);
 
-        int expectedTotalCount = 5; // 예상되는 총 결과 개수
+        // then
+        int expectedTotalCount = 3; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getContent().contains(keyword)));
     }
 
     @Test
-    public void findByStatusTest() {
-        int requesterId = 4;
-        String status = "WAITING";
+    public void findByStatusTest() throws NotLoginException {
+        // given
         int page = 1;
-        int size = 10;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        String status = "WAITING";
+        requesterSetUp();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByStatus(requesterId, status, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findByStatus(status, page, size);
 
+        // then
         int expectedTotalCount = 4; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getIsApproved().equals(status)));
     }
 
     @Test
-    public void findReceivedByContentTest() {
-        int approverId = 2;
-        String keyword = "content";
+    public void findReceivedByContentTest() throws NotLoginException {
+        // given
         int page = 1;
-        int size = 10;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        String keyword = "Content";
+        approverSetUp();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByContent(approverId, keyword, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByContent(keyword, page, size);
 
+        // then
         int expectedTotalCount = 3; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getContent().contains(keyword)));
     }
 
     @Test
-    public void findReceivedByStatusTest() {
-        int approverId = 2;
-        String status = "WAITING";
+    public void findReceivedByStatusTest() throws NotLoginException {
+        // given
         int page = 1;
-        int size = 10;
+        int size = 10; // 한 페이지에 표시할 결과의 최대 개수
+        String status = "WAITING";
+        approverSetUp();
 
-        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByStatus(approverId, status, page, size);
+        // when
+        List<QryRequestApprovalInfoDTO> approvalInfoDTOS = qryRequestApprovalService.findReceivedByStatus(status, page, size);
 
-        int expectedTotalCount = 1; // 예상되는 총 결과 개수
+        // then
+        int expectedTotalCount = 4; // 예상되는 총 결과 개수
         assertEquals(expectedTotalCount, approvalInfoDTOS.size());
         assertTrue(approvalInfoDTOS.stream().allMatch(dto -> dto.getIsApproved().equals(status)));
     }
 
+
+    private void addHrManagerForTest() {
+        ManageUserDTO userDTO = new ManageUserDTO(TestUserInfo.HR_MANAGER_EMAIL, TestUserInfo.PASSWORD, TestUserInfo.NAME, null,
+                TestUserInfo.HR_MANAGER_ROLE, null, null, TestUserInfo.PHONE_NUMBER, 1, 1,
+                1);
+        managerService.signup(userDTO);
+    }
+
+    private void requesterSetUp() throws NotLoginException {
+        loggedInUser.setting();
+        addHrManagerForTest();
+
+        int requesterId = authService.whoAmI().getId();
+        int approverId = employeeRepository.findByEmail(TestUserInfo.HR_MANAGER_EMAIL).getId();
+
+        int typeId = 1;
+        String content = "Test Content";
+
+        CmdApprovalRequestDTO requestDto = new CmdApprovalRequestDTO();
+        requestDto.setApproverId(approverId);
+        requestDto.setTypeId(typeId);
+        requestDto.setContent(content);
+
+        int secondTypeId = 3;
+        String secondContent = "Test Approval";
+        CmdApprovalRequestDTO secondRequestDto = new CmdApprovalRequestDTO();
+        secondRequestDto.setApproverId(approverId);
+        secondRequestDto.setTypeId(secondTypeId);
+        secondRequestDto.setContent(secondContent);
+
+        // when
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(secondRequestDto);
+
+    }
+
+    private void approverSetUp() throws NotLoginException {
+        loggedInUser.setting();
+        addHrManagerForTest();
+
+        int approverId = authService.whoAmI().getId();
+
+        int typeId = 2;
+        String content = "Test Content";
+
+        CmdApprovalRequestDTO requestDto = new CmdApprovalRequestDTO();
+        requestDto.setApproverId(approverId);
+        requestDto.setTypeId(typeId);
+        requestDto.setContent(content);
+
+        int secondTypeId = 3;
+        String secondContent = "Test Approval";
+        CmdApprovalRequestDTO secondRequestDto = new CmdApprovalRequestDTO();
+        secondRequestDto.setApproverId(approverId);
+        secondRequestDto.setTypeId(secondTypeId);
+        secondRequestDto.setContent(secondContent);
+
+        // when
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(requestDto);
+        cmdRequestApprovalService.createApproval(secondRequestDto);
+    }
 }
