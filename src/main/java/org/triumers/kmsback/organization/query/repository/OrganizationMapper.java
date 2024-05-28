010a223340702cc9d37550882e330054e9f8d876
moomint8 <office05053@gmail.com>
2024-05-29 03:13:26 +0900

    feat(organization): 부서 조회 기능 구현
    부서를 조회하는 기능이 구현되었습니다.
    구현 목록은 다음과 같습니다.
    - 부서 id 검색
    - 부서명으로 검색
    - 상위 본부 id로 검색

--- src/main/java/org/triumers/kmsback/organization/query/repository/OrganizationMapper.java
+++ src/main/java/org/triumers/kmsback/organization/query/repository/OrganizationMapper.java
@@ -2,6 +2,7 @@ package org.triumers.kmsback.organization.query.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.triumers.kmsback.organization.query.aggregate.entity.QryCenter;
+import org.triumers.kmsback.organization.query.aggregate.entity.QryDepartment;
 
 import java.util.List;
 
@@ -11,4 +12,10 @@ public interface OrganizationMapper {
     QryCenter findCenterById(int id);
 
     List<QryCenter> findCenterListByName(String name);
+
+    QryDepartment findDepartmentById(int id);
+
+    List<QryDepartment> findDepartmentListByName(String name);
+
+    List<QryDepartment> findDepartmentListByCenterId(int centerId);
 }
