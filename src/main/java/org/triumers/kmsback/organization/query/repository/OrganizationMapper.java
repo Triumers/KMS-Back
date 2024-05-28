c23f4601b5340c3436431ca3626f91b61e28d80a
moomint8 <office05053@gmail.com>
2024-05-28 21:30:25 +0900

    feat(organization): 본부 이름으로 검색 기능 구현
    본부 이름을 이용해 검색하는 기능이 구현되었습니다.

--- src/main/java/org/triumers/kmsback/organization/query/repository/OrganizationMapper.java
+++ src/main/java/org/triumers/kmsback/organization/query/repository/OrganizationMapper.java
@@ -3,8 +3,12 @@ package org.triumers.kmsback.organization.query.repository;
 import org.apache.ibatis.annotations.Mapper;
 import org.triumers.kmsback.organization.query.aggregate.entity.QryCenter;
 
+import java.util.List;
+
 @Mapper
 public interface OrganizationMapper {
 
     QryCenter findCenterById(int id);
+
+    List<QryCenter> findCenterListByName(String name);
 }
