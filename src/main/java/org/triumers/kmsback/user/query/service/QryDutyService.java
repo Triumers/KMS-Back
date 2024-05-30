353534ad7ee7ec2df2579ca45bb04541eda2a5d4
moomint8 <office05053@gmail.com>
2024-05-30 14:19:44 +0900

    test(manager): 직급, 직책 조회 기능 테스트 구현
    직급과 직책을 전체 조회, 이름 검색하는 테스트를 구현했습니다.

---
+++ src/main/java/org/triumers/kmsback/user/query/service/QryDutyService.java
@@ -0,0 +1,17 @@
+package org.triumers.kmsback.user.query.service;
+
+import org.triumers.kmsback.user.query.dto.QryPositionDTO;
+import org.triumers.kmsback.user.query.dto.QryRankDTO;
+
+import java.util.List;
+
+public interface QryDutyService {
+
+    List<QryPositionDTO> findAllPosition();
+
+    List<QryPositionDTO> findPositionByName(String name);
+
+    List<QryRankDTO> findAllRank();
+
+    List<QryRankDTO> findRankByName(String name);
+}
