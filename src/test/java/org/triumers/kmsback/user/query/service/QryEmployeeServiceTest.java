370c87ecce223bb1eab49bd7431aaf8ae6e21f37
moomint8 <office05053@gmail.com>
2024-05-30 20:54:05 +0900

    test(user): 직원 조회 기능 테스트 구현
    직원을 조회하는 기능 테스트가 구현되었습니다.
    - 직원id로 조회
    - 전체 직원 조회
    - email로 직원 조회
    - 이름으로 직원 조회
    - TeamId로 직원 조회

---
+++ src/test/java/org/triumers/kmsback/user/query/service/QryEmployeeServiceTest.java
@@ -0,0 +1,120 @@
+package org.triumers.kmsback.user.query.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.triumers.kmsback.common.LoggedInUser;
+import org.triumers.kmsback.common.TestUserInfo;
+import org.triumers.kmsback.user.query.dto.QryEmployeeDTO;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QryEmployeeServiceTest {
+
+    @Autowired
+    private QryEmployeeService qryEmployeeService;
+
+    @Autowired
+    private LoggedInUser loggedInUser;
+
+
+    @DisplayName("직원 조회 by Id")
+    @Test
+    void findEmployeeById() {
+
+        // given
+        int employeeId = userSetting();
+
+        // when
+        QryEmployeeDTO result = qryEmployeeService.findEmployeeById(employeeId);
+
+        // then
+        assertNotNull(result);
+        assertTrue(isEqualEmployee(result));
+    }
+
+    @DisplayName("전체 직원 조회")
+    @Test
+    void findAllEmployee() {
+
+        // given
+        loggedInUser.setting();
+
+        // when
+        List<QryEmployeeDTO> result = qryEmployeeService.findAllEmployee();
+
+        // then
+        assertNotNull(result);
+    }
+
+    @DisplayName("직원 조회 by Email")
+    @Test
+    void findEmployeeByEmail() {
+
+        // given
+        loggedInUser.setting();
+
+        // when
+        QryEmployeeDTO result = qryEmployeeService.findEmployeeByEmail(TestUserInfo.EMAIL);
+
+        // then
+        assertNotNull(result);
+        assertTrue(isEqualEmployee(result));
+    }
+
+    @DisplayName("직원 조회 by name")
+    @Test
+    void findEmployeeByName() {
+
+        // given
+        loggedInUser.setting();
+
+        // when
+        List<QryEmployeeDTO> result = qryEmployeeService.findEmployeeByName(TestUserInfo.NAME);
+
+        // then
+        assertNotNull(result);
+        for (QryEmployeeDTO employee : result) {
+            assertTrue(employee.getName().contains(TestUserInfo.NAME));
+        }
+    }
+
+    @DisplayName("직원 조회 by TeamId")
+    @Test
+    void findEmployeeByTeamId() {
+
+        // given
+        loggedInUser.setting();
+
+        // when
+        List<QryEmployeeDTO> result = qryEmployeeService.findEmployeeByTeamId(1);
+
+        // then
+        assertNotNull(result);
+        for (QryEmployeeDTO employee : result) {
+            System.out.println("employee = " + employee);
+        }
+        assertFalse(result.isEmpty());
+    }
+
+    private int userSetting() {
+        loggedInUser.setting();
+        return qryEmployeeService.findEmployeeByEmail(TestUserInfo.EMAIL).getId();
+    }
+
+    private boolean isEqualEmployee(QryEmployeeDTO employee) {
+        if (!employee.getEmail().equals(TestUserInfo.EMAIL)) {
+            return false;
+        }
+        if (!employee.getName().equals(TestUserInfo.NAME)) {
+            return false;
+        }
+        return employee.getPhoneNumber().equals(TestUserInfo.PHONE_NUMBER);
+    }
+}
