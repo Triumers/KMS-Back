9342d3b96b2f3719bf12c2fcaa78615738d191b6
hso <sh07202@naver.com>
2024-06-03 14:58:34 +0900

    style(group) : 회원이 속한 탭 전체 조회 수정(id -> whoAmI()로 변경)

--- src/main/java/org/triumers/kmsback/group/query/service/QryGroupServiceImpl.java
+++ src/main/java/org/triumers/kmsback/group/query/service/QryGroupServiceImpl.java
@@ -2,8 +2,11 @@ package org.triumers.kmsback.group.query.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.triumers.kmsback.common.exception.NotLoginException;
 import org.triumers.kmsback.group.query.dto.QryGroupDTO;
 import org.triumers.kmsback.group.query.repository.GroupMapper;
+import org.triumers.kmsback.user.command.Application.service.AuthService;
+import org.triumers.kmsback.user.command.domain.aggregate.entity.Employee;
 
 import java.util.List;
 
@@ -12,15 +15,19 @@ public class QryGroupServiceImpl implements QryGroupService {
 
     @Autowired
     private final GroupMapper groupMapper;
+    private final AuthService authService;
 
     @Autowired
-    public QryGroupServiceImpl(GroupMapper groupMapper) {
+    public QryGroupServiceImpl(GroupMapper groupMapper, AuthService authService) {
         this.groupMapper = groupMapper;
+        this.authService = authService;
     }
 
     @Override
-    public List<QryGroupDTO> findGroupByEmployeeId(int employeeId) {
-        return groupMapper.findGroupByEmployeeId(employeeId);
+    public List<QryGroupDTO> findGroupByEmployeeId() throws NotLoginException {
+
+        Employee employee = authService.whoAmI();
+        return groupMapper.findGroupByEmployeeId(employee.getId());
     }
 
     @Override
