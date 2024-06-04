a8952aede2f7eeba81f47e6c53e1a99299502ef7
moomint8 <office05053@gmail.com>
2024-06-04 11:47:29 +0900

    feat(employee): 직원 조회 기능 Controller 구현
    직원을 조회하는 기능 Controller가 구현되었습니다.
    - 직원 Id 조회
    - 전체 조회
    - 이메일 조회
    - 이름 조회
    - 팀 Id 조회

---
+++ src/main/java/org/triumers/kmsback/user/query/aggregate/vo/QryRequestEmployeeVO.java
@@ -0,0 +1,11 @@
+package org.triumers.kmsback.user.query.aggregate.vo;
+
+import lombok.Getter;
+
+@Getter
+public class QryRequestEmployeeVO {
+    private int id;
+    private String email;
+    private String name;
+    private int teamId;
+}
