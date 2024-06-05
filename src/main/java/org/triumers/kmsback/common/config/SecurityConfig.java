02778d100d7239d1eedead12bbd37b4f8bc1858b
moomint8 <office05053@gmail.com>
2024-06-05 10:17:57 +0900

    feat(auth): feat health check for securityConfig
    soon release will delete permitAll so feat this method

--- src/main/java/org/triumers/kmsback/common/config/SecurityConfig.java
+++ src/main/java/org/triumers/kmsback/common/config/SecurityConfig.java
@@ -104,6 +104,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/**").permitAll()
                 .requestMatchers("/auth/check-authenticator").permitAll()
+                .requestMatchers("/health-check").permitAll()
                 .requestMatchers("/manager/*").hasAnyRole("HR_MANAGER")
                 .anyRequest().authenticated());
 
