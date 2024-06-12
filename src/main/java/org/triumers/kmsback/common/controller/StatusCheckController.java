2ecded0e9ae4e448de874e5604449beb0dd10eb9
moomint8 <office05053@gmail.com>
2024-06-12 12:24:03 +0900

    fix(auth): 외부망 접속 시 2차 인증 버그 수정위한 컨트롤러

--- src/main/java/org/triumers/kmsback/common/controller/StatusCheckController.java
+++ src/main/java/org/triumers/kmsback/common/controller/StatusCheckController.java
@@ -1,6 +1,7 @@
 package org.triumers.kmsback.common.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,9 @@ import org.triumers.kmsback.common.util.IpAddressUtil;
 
 @RestController
 public class StatusCheckController {
+
+    @Value("${in-house-ip-address}")
+    private String defaultIpAddress;
 
     @GetMapping("/health-check")
     public ResponseEntity<String> checkHealthStatus() {
@@ -20,5 +24,12 @@ public class StatusCheckController {
     public ResponseEntity<String> checkIpAddressCheck(HttpServletRequest request) {
 
         return ResponseEntity.status(HttpStatus.OK).body("Current Ip Address: " + IpAddressUtil.getClientIp(request));
+    }
+
+    @GetMapping("/is-in-house")
+    public ResponseEntity<String> isInHouse(HttpServletRequest request) {
+        String ipAddress = IpAddressUtil.getClientIp(request);
+        String inHouseIpAddress = defaultIpAddress;
+        return ResponseEntity.status(HttpStatus.OK).body("current: " + ipAddress + " / in-house: " + inHouseIpAddress + " / is same?: " + ipAddress.equals(inHouseIpAddress));
     }
 }
